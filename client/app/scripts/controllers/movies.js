c4aa95e23fbbbf930eab1025dc99a0d5491edcb4
mufniarz <marcinmu330@gmail.com>
2016-07-11 03:48:57 -0500

    added to the view

---
+++ client/app/scripts/controllers/movies.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name clientApp.controller:MoviesCtrl
+ * @description
+ * # MoviesCtrl
+ * Controller of the clientApp
+ */
+angular.module('clientApp')
+  .controller('MoviesCtrl', function ($scope, Movie) {
+    $scope.movies = Movie.getList().$object;
+    /*[
+      {
+      	title: "The Punisher",
+      	url: "www.google.com"
+      },
+      {
+      	title: "Harry Potter",
+      	url: "www.twitter.com"
+      }
+    ];*/
+  });
